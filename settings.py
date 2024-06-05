7fb91e3316ee076392ff6c9d358188ced4da6128
Pawarit Chayawiwatkul <pawaritpawaritts@Pawarits-MacBook-Air-2.local>
2024-06-05 10:21:46 +0700

    [SET] for AWS

--- settings.py
+++ settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 # import environ
-from google.cloud import secretmanager
 import os
 import io
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
